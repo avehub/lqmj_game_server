4542ea29b2db3947577066380e19fe7d81e565df
张一工 <1148971617@qq.com>
2025-05-22 10:32:23 +0800

    类方法

--- c_services/const/base_card.py
+++ c_services/const/base_card.py
@@ -24,7 +24,8 @@ class BaseCard(IntEnum):
         """是否包含指定值"""
         return cls._value2member_map_.get(value)
 
-    def all_cards(self):
+    @classmethod
+    def all_cards(cls):
         raise NotImplementedError
 
     @staticmethod
