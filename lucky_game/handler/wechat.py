6ecf77b31e51e6196beaa7cffe79a6d6e128a521
大树 <avehub@163.com>
2025-08-14 16:04:08 +0800

    Fix 微信H5登录日志

--- lucky_game/handler/wechat.py
+++ lucky_game/handler/wechat.py
@@ -8,6 +8,7 @@ from nsanic.libs.tool import http_get, http_post, json_parse, json_encode
 from nsanic.libs import tool_dt
 from common.utils.utils import UtilsTool
 from lucky_game.model_rc.base_user import BaseUserRC
+from nsanic.libs.mult_log import NLogger
 
 
 class WeChat(LogMeta):
@@ -69,7 +70,9 @@ class WeChat(LogMeta):
 
         url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={}&secret={}&code={}&grant_type=authorization_code&connect_redirect=1"
         url = url.format(gzh_id, gzh_secret, code)
+        NLogger.info(f"微信H5登录获取用户授权url：{url}")
         req_data = await http_get(url)
+        NLogger.info(f"微信H5登录获取获取授权结果：{req_data}")
         return cls.__return_req_data(req_data)
 
     @classmethod
