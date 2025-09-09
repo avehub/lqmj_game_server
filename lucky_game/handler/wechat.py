e0eff0ad185aa4795d50c75a8de5285a961e14d8
大树 <avehub@163.com>
2025-09-09 12:30:25 +0800

    Fix 小程序支付调整为正式环境

--- lucky_game/handler/wechat.py
+++ lucky_game/handler/wechat.py
@@ -228,9 +228,9 @@ class WeChat(LogMeta):
         session_key = session_key.decode("utf-8")
         env = 0
         app_key = WeChatConf.WE_CHAT_APP_KEY
-        if ENV != "prod":
-            env = 1
-            app_key = WeChatConf.WE_CHAT_APP_KEY_DEV
+        # if ENV != "prod":
+        #     env = 1
+        #     app_key = WeChatConf.WE_CHAT_APP_KEY_DEV
         sign_data = {
             "mode": 'goods',  # game 游戏币 / goods 道具直购
             "offerId": WeChatConf.WE_CHAT_MG_OFFER_ID,
