1ca8fd90444623b89e5047e44816e138f264d6a4
方逢禄 <1328776471@QQ.COM>
2025-09-11 20:20:37 +0800

    复活玩家金币日志

--- c_services/base/base_leisure_room.py
+++ c_services/base/base_leisure_room.py
@@ -489,6 +489,7 @@ class BaseLeisureRoom(BaseRoom):
         """ 玩家充值回调 """
         self.log_info("收到玩家复活",player.uid,player.seat_id)
         await self.service.init_player(player)
+        self.log_info("收到玩家复活gold",player.gold)
         if player.gold <= 0:
             return await self.inner_send(player, CmdRoom.RECHARGE, code=StaCode.GOLD_NOT_ENOUGH)
         if player.is_out:
