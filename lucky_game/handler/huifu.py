764e82aed7855e480cd0d0b6012fc7a45356705b
大树 <avehub@163.com>
2025-09-19 20:26:55 +0800

    Fix 修改配置文件

--- lucky_game/handler/huifu.py
+++ lucky_game/handler/huifu.py
@@ -9,7 +9,7 @@ from nsanic.libs.mult_log import NLogger
 from nsanic.libs.tool import json_parse
 
 from common.utils.kit_dt import KitDt
-from common.public.conf import WeChatConf, HuiFuConf, SERVER_ADDR, LIVE_SERVER
+from common.public.conf import WeChatConf, HuiFuConf, SERVER_ADDR, LIVE_SERVER, AliPayConf
 from lucky_game.const import OrderStatus, GainStatus, PlatForm
 
 RESPONSE_CODE = {
@@ -119,9 +119,8 @@ class DouGongPay:
         request.trans_amt = f"{float(order_info.get('amount')):.2f}"  # 交易金额，必须大于0，保留两位小数点，如0.10、100.05等
 
         # 准备extend_infos，包括所有需要额外传递的参数
-        server_addr = SERVER_ADDR
         extend_infos = {
-            "notify_url": f'{server_addr}/luckyGame/CallbackHf',  # 交易异步通知地址
+            "notify_url": SERVER_ADDR + AliPayConf.NOTIFY_URL,  # 交易异步通知地址
             "wx_data": wx_data
         }
         # 异步请求
