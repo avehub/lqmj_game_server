dfdf853a2cb424321f50d69ef032621c449dbbee
大树 <avehub@163.com>
2025-09-20 18:15:12 +0800

    Add 增加阿里云支付日志

--- lucky_game/logic/payment.py
+++ lucky_game/logic/payment.py
@@ -405,10 +405,6 @@ class PaymentLogic:
         u_info = await BaseUserRC.cache_by_pk(order["uid"])
         sta, msg, req_res = await WeChat.wechat_mini_game_query_order(u_info["uid"], u_info["openid"], order_no)
         NLogger.info(f"微信小程序平台订单查询: order_no {order_no} 状态: {sta} 结果: {msg} {req_res}")
-        # 特殊处理：复活礼包且支付成功直接领取奖励and发送消息给游戏服务
-        if sta and req_res.get("trade_status") == OrderStatus.PAID and order["sku"] in [GoodsSku.SKU_REVIVE_1,
-                                                GoodsSku.SKU_REVIVE_2, GoodsSku.SKU_REVIVE_3, GoodsSku.SKU_REVIVE_4]:
-            await FirstCharge().charge_order(order)
         return sta, msg, req_res
 
 
