c8a37f6105e9a5ad4ac574ca97e72e214c63508d
平 韦 <avehub@163.com>
2025-10-15 17:18:11 +0800

    Dev 韦平

--- lucky_game/interface/club_group.py
+++ lucky_game/interface/club_group.py
@@ -12,7 +12,7 @@ class CreatGroup(GameAuthApi):
     """创建隔离组"""
     async def post(self, req: Request, **kwargs):
         uid = kwargs.get("u_info").get("uid")
-        u_ids = self.check_str(req.json.get("u_ids"), require=True, p_name="被隔离用户ID")
+        u_ids = self.check_str(req.json.get("u_ids"), require=False, p_name="被隔离用户ID")
         club_id = self.check_int(req.json.get("club_id"), require=True, p_name="茶馆ID")
         name = self.check_str(req.json.get("name"), maxlen=16, require=True, p_name="隔离组名")
         gid, e = await ClubGroupRC.creat_club_group(
