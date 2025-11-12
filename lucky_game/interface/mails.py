1bd5f952401b110594219c7d4044c37a24e451fa
大树 <avehub@163.com>
2025-11-12 18:25:43 +0800

    Fix 邮件列表

--- lucky_game/interface/mails.py
+++ lucky_game/interface/mails.py
@@ -17,10 +17,10 @@ from lucky_game.logic.activity import Base
 class MailsListHandler(GameAuthApi):
     """获取邮件列表"""
 
-    async def get(self, _, **kwargs):
+    async def get(self, req, **kwargs):
         user = kwargs.get("u_info")
         uid = user.get("uid")
-        sta, mail_list = await MailsRC.get_mails_list(uid=uid)
+        sta, mail_list = await MailsRC.get_mails_list(uid=uid, mail_sta=[MailSta.UNREAD, MailSta.READ])
         if sta:
             mail_list = await MailsRC.get_mail_awards(mail_list)
         return self.answer(data=mail_list)
--- lucky_game/model_rc/base_mails.py
+++ lucky_game/model_rc/base_mails.py
@@ -23,7 +23,7 @@ class MailsRC(BaseCommonRC):
 
     @classmethod
     async def get_mails_list(cls, uid: int = None, start_time: int = None, end_time: int = None, page: int = None,
-                             page_size: int = None, order_field: str = None, exp_time: int = None):
+                             page_size: int = None, order_field: str = None, exp_time: int = None, mail_sta: any = None):
         """获取邮件列表"""
         try:
             query = {}
@@ -33,6 +33,11 @@ class MailsRC(BaseCommonRC):
                 query["created__gte"] = start_time
             if end_time is not None:
                 query["created__lt"] = end_time
+            if mail_sta is not None:
+                if isinstance(mail_sta, list):
+                    query["mail_sta__in"] = mail_sta
+                else:
+                    query["mail_sta"] = mail_sta
             if order_field is None:
                 order_field = "-mail_id"
 
