917fb56f5ef2de32d4d95c8c624f076947679c07
平 韦 <avehub@163.com>
2025-11-19 19:36:42 +0800

    Fix 写入茶馆行为记录优化

--- lucky_game/model_rc/club_users.py
+++ lucky_game/model_rc/club_users.py
@@ -106,7 +106,7 @@ class ClubUsersRC(BaseCommonRC):
                         ExtraClubBehaviorRC.BEHAVIOR_MANAGE_INDEX,
                         data["uid"],
                         data["club_id"],
-                        check_uid,
+                        check_uid=check_uid,
                         status=status
                     )
                     if not sta:
@@ -143,7 +143,7 @@ class ClubUsersRC(BaseCommonRC):
                     ExtraClubBehaviorRC.BEHAVIOR_OUT_INDEX,
                     data.uid,
                     data.club_id,
-                    0 if check_uid == data.uid else check_uid,  # 主动离开check_id=0
+                    check_uid=0 if check_uid == data.uid else check_uid,  # 主动离开check_id=0
                     status=ExtraClubBehaviorRC.BEHAVIOR_STATUS_SUCCEED,
                 )
                 if not sta:
@@ -291,7 +291,7 @@ class ClubUsersRC(BaseCommonRC):
                         ExtraClubBehaviorRC.BEHAVIOR_BLACK_INDEX,
                         uid,
                         club_id,
-                        check_uid,
+                        check_uid=check_uid,
                         status=ExtraClubBehaviorRC.BEHAVIOR_STATUS_SUCCEED
                     )
                     if not sta:
@@ -317,7 +317,7 @@ class ClubUsersRC(BaseCommonRC):
                     ExtraClubBehaviorRC.BEHAVIOR_BLACK_INDEX,
                     club_user["uid"],
                     club_user["club_id"],
-                    check_uid,
+                    check_uid=check_uid,
                     status=ExtraClubBehaviorRC.BEHAVIOR_STATUS_CANCEL,
                 )
                 if not sta:
