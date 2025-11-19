b0c4d7b0874ccfec4ed5cad71e1678122b5e36f5
方逢禄 <1328776471@QQ.COM>
2025-11-19 20:47:43 +0800

    休闲场退出房间

--- c_services/base/base_leisure_room.py
+++ c_services/base/base_leisure_room.py
@@ -124,7 +124,8 @@ class BaseLeisureRoom(BaseRoom):
             player.tid = 0  # 清除玩家房间号 防止check in table时房间号错乱
             self.log_info(player.uid, "玩家认输退出房间", id(player))
             await self.try_round_over()
-            await self.service.del_player_in_service(player.uid)
+            if self.service:
+                await self.service.del_player_in_service(player.uid)
 
     async def try_round_over(self):
         """ 尝试解散房间 """
@@ -136,7 +137,7 @@ class BaseLeisureRoom(BaseRoom):
         if self.room_status == RoomStatus.T_IDLE:
             return
         self.log_info("房间内已经没有真人玩家，enter force_dismiss")
-        await self.delay_func(0.5, self.force_dismiss)
+        await self.force_dismiss()
 
     async def deduct_tickets(self):
         """ 扣除门票 """
