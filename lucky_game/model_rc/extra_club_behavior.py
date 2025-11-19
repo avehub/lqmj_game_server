3bdb1bee5d41dd498c2e4ab1de73acf21b8a6310
大树 <avehub@163.com>
2025-11-19 21:02:28 +0800

    Fix 茶馆用户行为记录隔离组展示问题优化

--- lucky_game/model_rc/extra_club_behavior.py
+++ lucky_game/model_rc/extra_club_behavior.py
@@ -242,11 +242,14 @@ class ExtraClubBehaviorRC(BaseCommonRC):
                 explain = f"{check_info['name']}将{u_info['name']}{msg}黑名单"
             # 隔离组
             elif item["type"] == cls.BEHAVIOR_ISOLATION_INDEX:
-                msg = "加入"
-                if item["status"] == cls.BEHAVIOR_STATUS_ALTER:
+                explain = f"{check_info['name']}操作了隔离组"
+                if item["status"] == cls.BEHAVIOR_STATUS_SUCCEED:
+                    msg = "加入"
+                    explain = f"{check_info['name']}将{u_info['name']}{msg}隔离组"
+                elif item["status"] == cls.BEHAVIOR_STATUS_ALTER:
                     msg = "更新至"
-                explain = f"{check_info['name']}将{u_info['name']}{msg}隔离组"
-                if item["status"] == cls.BEHAVIOR_STATUS_CANCEL:
+                    explain = f"{check_info['name']}将{u_info['name']}{msg}隔离组"
+                elif item["status"] == cls.BEHAVIOR_STATUS_CANCEL:
                     msg = "删除了"
                     explain = f"{check_info['name']}{msg}隔离组"
             # 退出茶馆
