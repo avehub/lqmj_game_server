0b85c61acfef390ff1645460ba7b0d8a47166569
大树 <avehub@163.com>
2025-11-22 09:19:27 +0800

    Fix 禁止同桌问题处理

--- lucky_game/model_rc/club_user_group.py
+++ lucky_game/model_rc/club_user_group.py
@@ -108,18 +108,17 @@ class ClubUserGroupRC(RCModel):
         try:
             sta = room_u_sta = False
             groups, _ = await cls.get_club_user_group_by_filter(club_id, uid=uid)
-            cls.conf.log.info(f"groups_1: {groups}, uid: {uid}, room_uid: {room_uid}")
+            cls.conf.log.info(f"groups_1: {groups}, 待加入uid: {uid}, 房间内room_uid: {room_uid}")
             if groups:
                 sta = await cls.__check_in_group(groups[0], uid, room_uid)
             if not sta:
-                for u_id in room_uid:
-                    group_list, _ = await cls.get_club_user_group_by_filter(club_id, u_ids=u_id)
-                    if group_list:
-                        for group in group_list:
-                            cls.conf.log.info(f"group_2: {group}, u_id: {u_id}, room_uid: {room_uid}")
-                            if uid == group["uid"]:
-                                room_u_sta = True
-                                break
+                group_list, _ = await cls.get_club_user_group_by_filter(club_id, u_ids=uid)
+                if group_list:
+                    for group in group_list:
+                        cls.conf.log.info(f"group_2: {group}, 待加入uid: {uid}, 房间内room_uid: {room_uid}")
+                        if uid == group["uid"]:
+                            room_u_sta = True
+                            break
         except OperationalError as e:
             return False
         return True if sta or room_u_sta else False
