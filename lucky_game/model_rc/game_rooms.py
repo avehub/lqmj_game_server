6d6ffb2634133de0b17963ea0281dd1701391c45
大树 <avehub@163.com>
2025-11-22 10:18:53 +0800

    Fix 禁止同桌问题处理

--- lucky_game/model_rc/game_rooms.py
+++ lucky_game/model_rc/game_rooms.py
@@ -240,6 +240,8 @@ class GameRoomsRC(BaseCommonRC):
     async def check_user_group(cls, room_data: dict, uid: int):
         """检查用户是否与房间成员在禁止同桌配置"""
         room_uid = await cls.conf.rds.smembers(f"{cls.SESSION_DISK_KEY}:{room_data['room_id']}")
+        cls.conf.log.info("redis集合房间内玩家", type(room_uid), room_uid)
+        cls.conf.log.info("用户列表", [p.decode('utf-8') for p in room_uid])
         exist = await ClubUserGroupRC.check_uid_by_room(
             room_data["club_id"],
             uid,
