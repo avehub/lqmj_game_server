b2eb0d9a189aadbf7728172ccad73a95cd645100
大树 <avehub@163.com>
2025-11-22 11:04:41 +0800

    Fix 禁止同桌问题处理

--- lucky_game/model_rc/club_user_group.py
+++ lucky_game/model_rc/club_user_group.py
@@ -116,7 +116,9 @@ class ClubUserGroupRC(RCModel):
                 if group_list:
                     for group in group_list:
                         cls.conf.log.info(f"group_2: {group}, 待加入uid: {uid}, 房间内room_uid: {room_uid}")
-                        room_u_sta = group["status"] == 1 and str(uid) in room_uid
+                        cls.conf.log.info(f"group_2:禁止同桌uid: {type(uid)} {uid}")
+                        cls.conf.log.info(f"group_2:禁止同桌room_uid: {type(room_uid)} {room_uid}")
+                        room_u_sta = group["status"] == 1 and uid in room_uid
                         if room_u_sta:
                             break
         except OperationalError as e:
