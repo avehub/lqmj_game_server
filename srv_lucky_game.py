e6c9fb5e8aa94354deb3b47c2d99eb5a255e5a73
大树 <avehub@163.com>
2025-11-28 11:27:19 +0800

    Add 后台首页聚合数据

--- srv_lucky_game.py
+++ srv_lucky_game.py
@@ -8,6 +8,9 @@ from lucky_game.url_main import MainBp
 from sanic import Sanic
 from common.utils.exceptions import global_exception_handler
 import sys
+import logging
+import asyncio
+import os
 
 signal_map = {}
 
@@ -21,3 +24,32 @@ if conf.FILE_UPLOAD.LOCAL_STORAGE['enable']:
 
 if __name__ == '__main__' or 'pydevd' in sys.modules:
     main_server.run()
+
+def setup_debug_logging(logfile: str = "tortoise_debug.log"):
+    # 根 logger
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(logfile, encoding="utf-8")
+        ],
+    )
+    # 明确开启这几个 logger 的 DEBUG
+    logging.getLogger("tortoise").setLevel(logging.DEBUG)
+    logging.getLogger("tortoise.db_client").setLevel(logging.DEBUG)
+    logging.getLogger("tortoise.queryset").setLevel(logging.DEBUG)
+    logging.getLogger("aiomysql").setLevel(logging.DEBUG)
+    logging.getLogger("asyncmy").setLevel(logging.DEBUG)
+    logging.getLogger("pymysql").setLevel(logging.DEBUG)  # 若用 pymysql
+    # asyncio debug
+    os.environ.setdefault("PYTHONASYNCIODEBUG", "1")
+    try:
+        # 在运行时也可以开启
+        asyncio.get_event_loop().set_debug(True)
+    except Exception:
+        pass
+
+# 在 app 启动时调用
+setup_debug_logging()
+
