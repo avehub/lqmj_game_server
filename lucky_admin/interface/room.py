f19895bc37c067810791bc4fb69b2f479c5d86cf
平 韦 <avehub@163.com>
2025-12-09 17:26:47 +0800

    Fix 历史战绩接口异常优化

--- lucky_admin/interface/room.py
+++ lucky_admin/interface/room.py
@@ -17,7 +17,7 @@ class GameRoom(AdminAuthApi):
         return self.answer(data=data, hint=msg)
 
 class GameRecord(AdminAuthApi):
-    async def get(self, req: Request, **kwargs):
+    async def get(self, req: Request):
         """房间战绩"""
         try:
             room_id = self.check_int(req.args.get('room_id'), require=False, p_name='房间ID')
@@ -26,10 +26,10 @@ class GameRecord(AdminAuthApi):
             page_size = self.check_int(req.args.get('page_size'), require=False, default=10, p_name='每页数量')
             start_time = self.check_int(req.args.get('start_time'), require=False, p_name='开始时间')
             end_time = self.check_int(req.args.get('end_time'), require=False, p_name='结束时间')
-            data, msg = await BaseRecordsGameRC.get_record_list(uid=uid, room_id=room_id, page=page,
-                                                                page_size=page_size,
-                                                                start_time=start_time, end_time=end_time)
+            data, msg = await BaseRecordsGameRC.get_record_list(uid=uid, room_id=room_id, page=page, page_size=page_size,
+                                                            start_time=start_time, end_time=end_time)
         except Exception as e:
-            return self.answer(self.sta_code.FAIL, hint=str(e))
+            self.log_info(f"获取房间战绩失败: {str(e)}")
+            return self.answer(self.sta_code.FAILURE, hint="获取房间战绩失败")
         return self.answer(data=data, hint=msg)
 
