1afecf238e4dfc0d769aaa0dde624b4bb27c94c0
方逢禄 <1328776471@QQ.COM>
2025-12-19 14:40:56 +0800

    比赛游戏配置RC

---
+++ lucky_game/model_rc/conf_competition.py
@@ -0,0 +1,20 @@
+"""
+赛事配置RC类
+"""
+from tortoise.exceptions import OperationalError
+from lucky_game.model_db.main import ConfCompetition
+from lucky_game.model_rc.base_rc import BaseCommonRC
+from lucky_game.const import CompetitionStatus
+from nsanic.libs.tool import json_encode
+
+
+class ConfCompetitionRC(BaseCommonRC):
+    """赛事配置模型RC类"""
+    db_model = ConfCompetition
+    tb_name = db_model.sheet_name()
+    cache_key = "conf_competition"
+
+    @classmethod
+    async def cache_conf_data_by_pk(cls, competition_id) -> dict:
+        db_conf_data = await cls.cache_by_pk(competition_id) or {}
+        return db_conf_data
