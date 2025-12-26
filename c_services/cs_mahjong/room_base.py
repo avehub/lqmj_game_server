d43c332fe3e3db87f8f599c4039fd590973ff34c
方逢禄 <1328776471@QQ.COM>
2025-12-26 17:40:16 +0800

    机器人报听出牌处理

--- c_services/cs_mahjong/room_base.py
+++ c_services/cs_mahjong/room_base.py
@@ -1034,7 +1034,7 @@ class Room(BaseCardRoom):
             data["lock_cards"] = p.lock_cards
             data_model = S2CTurnToMahjong.pb_model(**data)
             await self.inner_send(p, CmdRoom.TURN_TO, data_model)
-            if self.__have_men_jian_hu and (p.all_chu_cards or p.cards_len == 5 or not contains_tian_ting):
+            if self.__have_men_jian_hu and (p.all_chu_cards or p.cards_len == 5 or not contains_tian_ting or p.is_robot):
                 return self.call_flow(0.5, self.robot_play_card_by_suo_pai, p)
         else:
             await self.inner_send(p, CmdRoom.TURN_TO, data_model)
