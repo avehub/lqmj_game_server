3bbcacea5cdb999ed588be2f9e4f9320178c8733
大树 <avehub@163.com>
2025-12-30 15:07:34 +0800

    Fix 后台接口问题优化

--- lucky_admin/config/conf_start.py
+++ lucky_admin/config/conf_start.py
@@ -71,6 +71,7 @@ class ConfSrv(BaseConf):
     @classmethod
     def makeup_db_conf(cls, model_list: list):
         model_list.append("lucky_proxy.model_db.main")
+        model_list.append("lucky_game.model_db.main")
         return {
             'apps': {
                 cls.SERVER_NAME: {'models': model_list},
