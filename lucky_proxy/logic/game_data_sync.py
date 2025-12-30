31937d019002dce53c6da294327ffb60d04fa8d1
tanlei <t18762405182@126.com>
2025-12-30 14:48:47 +0800

    edit: 订单bug sql修正

--- lucky_proxy/logic/game_data_sync.py
+++ lucky_proxy/logic/game_data_sync.py
@@ -147,8 +147,7 @@ class GameDataSync(LogMeta):
         if order_type == 1:
             sql = f"""
                  update proxy_user_wallet 
-                       
-                        total_amount=total_amount+{str(order_amount)}
+                        set total_amount=total_amount+{str(order_amount)}
                         ,  total_income=total_income+{str(proxy_income)}
                         ,  room_amount=room_amount+{str(order_amount)}
                         ,  room_income=room_income+{str(proxy_income)}
@@ -159,7 +158,7 @@ class GameDataSync(LogMeta):
         if order_type == 2:
             sql = f"""
                  update proxy_user_wallet 
-                         total_amount=total_amount+{str(order_amount)}
+                         set total_amount=total_amount+{str(order_amount)}
                         ,  total_income=total_income+{str(proxy_income)}
                         ,  assistance_program_amount=assistance_program_amount+{str(order_amount)}
                         ,  assistance_program_income=assistance_program_income+{str(proxy_income)}
