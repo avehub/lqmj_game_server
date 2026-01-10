dddfac4bcda97b4f67cb20cb889e9d964e6ef22a
tanlei <t18762405182@126.com>
2026-01-10 10:30:33 +0800

    edit: 升级一级代理 房卡固定分佣给原代理

--- lucky_proxy/doc/sql/20260108.update.sql
+++ lucky_proxy/doc/sql/20260108.update.sql
@@ -4,3 +4,5 @@ ALTER TABLE proxy_user ADD COLUMN upgrade_time BIGINT COMMENT '升级一级代�
 ALTER TABLE proxy_user ADD COLUMN opt_user_id BIGINT COMMENT '操作用户id';
 
 ALTER TABLE proxy_promotion_relation ADD COLUMN upgrade_flag tinyint  default 0 COMMENT '升级成一级代理标志1、已升级 0、未生级';
+
+ALTER TABLE proxy_user_wallet ADD COLUMN upgrade_flag tinyint  default 0 COMMENT '升级成一级代理标志1、已升级 0、未生级';
