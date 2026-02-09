ff7c3d7336befad13e5499f3d16232ad4ece7a44
程国强 <345818317@qq.com>
2026-02-09 14:26:03 +0800

    fix  DistributionAgriOrders status

--- lucky_admin/interface/distribution.py
+++ lucky_admin/interface/distribution.py
@@ -168,9 +168,9 @@ class DistributionAgriOrders(AdminAuthApi):
             where += f" AND exchange_no = '{order_no}'"
         if tracking_no:
             where += f" AND express_no = '{tracking_no}'"
-        if status_k == "shipped":
+        if status_k == "1":
             where += f" AND express_no <> ''"
-        elif status_k == "unshipped":
+        elif status_k == "0":
             where += f" AND (express_no = '' OR express_no = '0' OR express_no IS NULL)"
         if start is not None:
             where += f" AND updated >= {start}"
