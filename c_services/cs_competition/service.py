ae91ac154c2325e7519ad7e2c0b80c32aff60830
方逢禄 <1328776471@QQ.COM>
2026-03-02 09:46:40 +0800

    正式赛获取比赛奖励

--- c_services/cs_competition/service.py
+++ c_services/cs_competition/service.py
@@ -85,8 +85,7 @@ class CompetitionServer(BaseServer):
             self.__current_cycle_type = cycle_info.get("cycle_type")
             reward_id = 3
             if self.__current_cycle_type != 1:
-                reward_id = 0
-                self.__reward_info = None
+                reward_id = 5
             sta, reward_info = await TournamentRewardRC.get_reward_info(reward_id)
             if sta:
                 self.__reward_info = self.build_rank_to_reward(reward_info)
