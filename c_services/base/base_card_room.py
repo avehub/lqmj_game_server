8980d3e9b16e83c7eb77547b886b89607b121aef
方逢禄 <1328776471@QQ.COM>
2026-03-12 09:17:29 +0800

    超时解散发调整

--- c_services/base/base_card_room.py
+++ c_services/base/base_card_room.py
@@ -59,9 +59,13 @@ class BaseCardRoom(BaseRoom):
     async def close_room_timeout_idle(self):
         idle_time = tool_dt.cur_time() - self.__create_time
         if self.game_began:
-            if idle_time >= self.__timeout_idle_time * 6:
-                self.log_info("游戏开始且超过6个小时，强制解散房间", self.in_room_count, self.seats,"时长",idle_time)
+            if idle_time >= self.__timeout_idle_time * 8:
                 await self.force_dismiss(OverType.ULTIMATE_DISMISS)
+                self.log_info("游戏开始且超过6个小时，强制解散房间", self.in_room_count, self.seats, "时长", idle_time)
+            elif idle_time >= self.__timeout_idle_time * 6:
+                await self.force_dismiss(OverType.FORCE)
+                self.log_info("游戏开始且超过6个小时，强制解散房间", self.in_room_count, self.seats,"时长",idle_time)
+
             return
         if idle_time < self.__timeout_idle_time:
             return
