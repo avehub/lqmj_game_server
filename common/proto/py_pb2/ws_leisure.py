3cae878941e0c60be4060d3bcaf47dd9d94dd484
方逢禄 <1328776471@QQ.COM>
2026-03-19 10:22:34 +0800

    跑得快玩家抢关状态

--- common/proto/py_pb2/ws_leisure.py
+++ common/proto/py_pb2/ws_leisure.py
@@ -1324,6 +1324,7 @@ class S2CPlayerInfoRunFast:
             p_info.is_ready = one_data.get("is_ready") or False
             p_info.total_score = one_data.get("total_score") or 0
             p_info.has_double = one_data.get("has_double") or False
+            p_info.has_qiang_guan = one_data.get("has_qiang_guan") or False
         return obj
 
 class S2CRoundStartRunFast:
